182b48b2ec1dda2f9417a9f06e8319278c278cf4
Mark <mac21macky@gmail.com>
2023-03-07 06:58:26 +0800

    Modify config

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 
-const API_KEY = 'ea4e7d4abb612db7a05195d4b37cca6c';
-const SUBSCRIBER_ID = '63dd93575fd0df47313ee933';
-const EMAIL = 'mac21macky@gmail.com';
+const API_KEY = '';
+const SUBSCRIBER_ID = '';
+const EMAIL = '';
 const PORT = 3000;
 
 export {
